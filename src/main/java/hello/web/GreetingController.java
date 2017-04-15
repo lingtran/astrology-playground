4ce7feac044c93d53eecab5d8c3d7c6df0e5aee7
lingtran <lxiutran@gmail.com>
2017-04-15 15:56:12 -0500

    Adjust build file; add new endpoint to GreetingController

--- src/main/java/hello/web/GreetingController.java
+++ src/main/java/hello/web/GreetingController.java
@@ -6,8 +6,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.Map;
 
@@ -30,6 +32,19 @@ public class GreetingController {
         model.put("msg", greetingService.getDesc());
 
         return "index";
+    }
+
+    @RequestMapping(value = "/hello/{name:.+}", method = RequestMethod.GET)
+    public ModelAndView hello(@PathVariable("name") String name) {
+        logger.debug("hello() is executed - $name {}", name);
+
+        ModelAndView model = new    ModelAndView();
+        model.setViewName("index");
+
+        model.addObject("title", greetingService.getTitle(name));
+        model.addObject("msg", greetingService.getDesc());
+
+        return model;
     }
 
 }
