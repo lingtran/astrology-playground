6db42ad39114e640006a5c4344ba64d78074d18b
lingtran <lxiutran@gmail.com>
2017-04-22 16:00:59 -0500

    Remove logger dependency to successfully debug rendering of HelloWorld page
    Remove logger dependency from build file and related files
    Add index.html

--- src/main/java/hello/web/GreetingController.java
+++ src/main/java/hello/web/GreetingController.java
@@ -2,8 +2,6 @@ package hello.web;
 
 
 import hello.service.GreetingService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,7 +14,6 @@ import java.util.Map;
 @Controller
 public class GreetingController {
 
-    private final Logger logger = LoggerFactory.getLogger(GreetingController.class);
     private final GreetingService greetingService;
 
     @Autowired
@@ -26,8 +23,6 @@ public class GreetingController {
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String index(Map<String, Object> model) {
-        logger.debug("index() is executed!");
-
         model.put("title", greetingService.getTitle(""));
         model.put("msg", greetingService.getDesc());
 
@@ -36,9 +31,7 @@ public class GreetingController {
 
     @RequestMapping(value = "/hello/{name:.+}", method = RequestMethod.GET)
     public ModelAndView hello(@PathVariable("name") String name) {
-        logger.debug("hello() is executed - $name {}", name);
-
-        ModelAndView model = new    ModelAndView();
+        ModelAndView model = new ModelAndView();
         model.setViewName("index");
 
         model.addObject("title", greetingService.getTitle(name));
--- src/main/webapp/WEB-INF/views/jsp/index.jsp
+++ src/main/webapp/WEB-INF/views/jsp/index.jsp
@@ -1,14 +1,38 @@
-<%@taglib prefix="spring" uri="http://www.springframework.org/tags" %>
-
+<%@ taglib prefix="spring" uri="http://www.springframework.org/tags"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>Gradle + Spring MVC</title>
+<title>Gradle + Spring MVC</title>
+
 </head>
 
-<body>
+
+  <div class="container">
+	<h1>${title}</h1>
+	<p>
+		<c:if test="${not empty msg}">
+			Hello ${msg}
+		</c:if>
+
+		<c:if test="${empty msg}">
+			Welcome Welcome!
+		</c:if>
+        </p>
+        <p>
+		<a class="btn btn-primary btn-lg"
+                    href="#" role="button">Learn more</a>
+	</p>
+	</div>
+
+
+
+  <hr>
+  <footer>
+	<p>© Mkyong.com 2015</p>
+  </footer>
+</div>
+
 
 </body>
 </html>
-
-
