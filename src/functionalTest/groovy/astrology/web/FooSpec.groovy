bd831f13ee5817090adfe0822022ddeaa4cb241b
Ling Tran <lxiutran@gmail.com>
2017-08-16 15:59:26 -0500

    WIP still figuring it out....

--- src/functionalTest/groovy/astrology/web/FooSpec.groovy
+++ src/functionalTest/groovy/astrology/web/FooSpec.groovy
@@ -13,16 +13,16 @@ class FooSpec extends GebSpec {
     // TODO: Figure out if is this the right setup?
     // TODO: System.setProperty to be done here? Need to clean up path
     def browser() {
-        System.setProperty("webdriver.chrome.driver", "/Users/ltran/Developer/Drivers/chromedriver")
+        System.setProperty("webdriver.chrome.driver", System.getProperty("user.home")+"//Developer/Drivers/chromedriver")
         new Browser(driver: new ChromeDriver())
     }
 //    TODO: Is this the best way to set up a baseUrl? Also overall is this the best structure for interacting with a Browser?
     def setupSpec(){
-        browser.getBaseUrl()
+        browser.config.properties.baseUrl
     }
 
-    @Ignore
     def "landing page"() {
+
         when:
         go "/"
 
